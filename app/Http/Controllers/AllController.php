fee90721c0b2a18753e91909b9dd9cdd72a2ec62
Zulma <zumendez7@gmail.com>
2021-06-01 23:59:33 +0200

    migration, seeder, model table = role, poste, genre, photo, team, testi, ready

--- app/Http/Controllers/AllController.php
+++ app/Http/Controllers/AllController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Discover;
 use App\Models\Logo;
 use App\Models\Service;
+use App\Models\Team;
 use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
@@ -16,8 +17,8 @@ class AllController extends Controller
         $services9 = Service::inRandomOrder()->limit(9)->get();
         $discovers = Discover::all();
         $testimonials = Testimonial::all();
-
-        return view('home', compact('logo', 'services3', 'services9', 'discovers', 'testimonials'));
+        $team = Team::all();
+        return view('home', compact('logo', 'services3', 'services9', 'discovers', 'testimonials', 'team'));
     }
     public function services(){
         return view('services');
